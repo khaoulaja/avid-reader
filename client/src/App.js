1a0fd1ef65956ecdd1c9fa31b9914655ef45b468
khaoula <khaoulajabour4@gmail.com>
2022-03-14 04:25:31 -0400

    install apollo client + create GET_ME query

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+//create a middleware function with setContext that will retrieve the token for us and combine it with the existing httpLink.
+const authLink = setContext((_, { headers }) => { //underscore _ to serve as a placeholder for the first parameter.
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+//we need to combine the authLink and httpLink objects so that every request retrieves the token and sets the request headers before making the request to the API. 
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={SearchBooks} />
+            <Route exact path='/saved' component={SavedBooks} />
+            
+            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+          </Switch>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
