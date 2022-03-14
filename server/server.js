973f745994213757e4abf593e9d2214099d600db
khaoula <khaoulajabour4@gmail.com>
2022-03-14 00:38:25 -0400

    create typedefs + create addUser and login mutations

--- server/server.js
+++ server/server.js
@@ -1,20 +1,41 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
+const {typeDefs, resolvers} = require('./schemas')
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const startServer = async ()=> {
+  //create new apollo server and pass in our schema
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+
+  //start Apollo server
+  await server.start();
+  //integrate apollo sessrver with express as middleware
+  server.applyMiddleware({ app });
+
+  console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+startServer();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
