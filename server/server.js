1b6887e96e0a8e077aacaf1900e7f1e3e37818a2
khaoula <khaoulajabour4@gmail.com>
2022-03-15 06:58:35 -0400

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -30,12 +30,12 @@ startServer();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-// app.use(routes);
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
